496df26f7f1857d8a36f2b6930273dc5aa3bd165
jaeyoun92 <jaeyoun92@gmail.com>
2023-09-15 17:08:28 +0900

    230915 다이어리 코드 복제 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:diary/dairy_service.dart';
+import 'package:diary/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'home_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // sharedPreferences 인스턴스 불러오기
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => DiaryService()),
+        ChangeNotifierProvider(create: (context) => DiaryService(prefs)),
       ],
       child: const MyApp(),
     ),
